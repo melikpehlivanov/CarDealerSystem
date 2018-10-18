70aef06e59fc515442dec644bf5058bd6379cdab
melik <melikpehlivanov@gmail.com>
2018-10-18 11:43:00 +0300

    Add LogService

---
+++ src/CarDealerSystem/CarDealer.Services/Implementations/Logs/LogService.cs
@@ -0,0 +1,57 @@
+﻿namespace CarDealer.Services.Implementations.Logs
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using CarDealer.Models;
+    using Data;
+    using Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using Models.Log;
+
+    public class LogService : BaseService, ILogService
+    {
+        private readonly IMapper mapper;
+
+        public LogService(CarDealerDbContext db, IMapper mapper) 
+            : base(db)
+        {
+            this.mapper = mapper;
+        }
+
+        public bool CreateUserActivityLog(UserActivityLogCreateModel model)
+        {
+            var log = this.mapper.Map<UserActivityLog>(model);
+
+            try
+            {
+                this.db.Logs.Add(log);
+                this.db.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public IQueryable<UserActivityLogConciseServiceModel> GetAll()
+        {
+            var logs = this.db
+                .Logs
+                .OrderByDescending(l => l.DateTime)
+                .ProjectTo<UserActivityLogConciseServiceModel>(this.mapper.ConfigurationProvider);
+
+            return logs;
+        }
+
+        public Task<UserActivityLogDetailsServiceModel> GetAsync(int id)
+            => this.db
+                .Logs
+                .Where(l => l.Id == id)
+                .ProjectTo<UserActivityLogDetailsServiceModel>(this.mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync();
+    }
+}
