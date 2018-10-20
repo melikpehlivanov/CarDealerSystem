ccc2cc934b18df0d9a1adff73906e5f8e724064e
melik <melikpehlivanov@gmail.com>
2018-10-20 11:50:44 +0300

    Added some more unit tests for AdService!

--- src/CarDealerSystem/CarDealer.Tests/Services/AdServiceTests.cs
+++ src/CarDealerSystem/CarDealer.Tests/Services/AdServiceTests.cs
@@ -250,6 +250,45 @@
         //}
 
         [Fact]
+        public void GetAdOwnerEmail_WithValidId_ShouldReturnCorrectUser()
+        {
+            // Arrange
+            const int id = 1;
+            this.dbContext.Ads.Add(new Ad { Id = id, IsDeleted = false, User = new User { Id = SampleUserId, Email = "SampleEmail", } });
+            this.dbContext.SaveChanges();
+
+            // Act
+            var result = this.adService.GetAdOwnerEmail(id);
+
+            // Assert
+            result
+                .Should()
+                .BeAssignableTo<string>();
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        public void GetAdOwnerEmail_WithInvalidId_ShouldReturnNull(int id)
+        {
+            // Arrange
+            this.dbContext.Ads.Add(new Ad { Id = 1, IsDeleted = false, User = new User { Id = SampleUserId, Email = "SampleEmail", } });
+            this.dbContext.Ads.Add(new Ad { Id = 2, IsDeleted = false, User = new User { Id = SampleUserId + 2, Email = "SampleEmail2", } });
+            this.dbContext.SaveChanges();
+
+            // Act
+            var result = this.adService.GetAdOwnerEmail(id);
+
+            // Assert
+            result
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
         public void GetAllAdsByOwnerId_ShouldReturnQueryWithValidModel()
         {
             // Arrange
